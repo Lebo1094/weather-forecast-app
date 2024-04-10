9cc6c6b7ab6518445612b14a8e7ecae30c7517a2
Lebo1094 <125014406+Lebo1094@users.noreply.github.com>
2024-04-10 18:44:55 +0200

    integrated the search engine and API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#city-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+  let city = searchInputElement.value;
+
+  function showWeather(response) {
+    let temperatureElement = document.querySelector("#temp-value");
+    temperatureElement.innerHTML = Math.round(
+      response.data.temperature.current
+    );
+    let humidityElement = document.querySelector("#humid");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+
+    cityElement.innerHTML = response.data.city;
+  }
+
+  let apiKey = "fd465o10bb292abbf346etb26bb03720";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}
+&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
 }
 
 let searchForm = document.querySelector("#city-form");

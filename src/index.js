700b2426c07e1cf1ae972cf39c2f1581950185c1
Lebo1094 <125014406+Lebo1094@users.noreply.github.com>
2024-04-11 10:32:45 +0200

    added more weather data and updated html and css

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ function search(event) {
 
   function showWeather(response) {
     let temperatureElement = document.querySelector("#temp-value");
+    let humidityElement = document.querySelector("#humid");
+    let descriptionElement = document.querySelector("#description");
+    let windElement = document.querySelector("#wind");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(
       response.data.temperature.current
     );
-    let humidityElement = document.querySelector("#humid");
     humidityElement.innerHTML = response.data.temperature.humidity;
-
+    descriptionElement.innerHTML = response.data.condition.description;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" /> `;
     cityElement.innerHTML = response.data.city;
   }
 
@@ -22,5 +28,36 @@ function search(event) {
   axios.get(apiUrl).then(showWeather);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#time");
+let currentDate = new Date();
+currentDateELement.innerHTML = formatDate(currentDate);
